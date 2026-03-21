096497360a770946a6b96f4734f41fb2ac06fa49
mkobier <m.kobierecki@wp.pl>
2026-03-21 10:44:52 +0100

    Vector2::Add() reversed

--- src/Game/Math/Vector2.cpp
+++ src/Game/Math/Vector2.cpp
@@ -2,6 +2,14 @@
 #include "../../Core/Memory.h"
 #include <iostream>
 
+Vector2* Vector2::Add(Vector2* other)
+{
+    this->x += other->x;
+    this->y += other->y;
+
+    return this;
+}
+
 Vector2* Vector2::Multiply(Vector2* result, Vector2* operand)
 {
     result->x = operand->x * this->x;
@@ -20,6 +28,7 @@ Vector2* Vector2::Sum(Vector2* result, Vector2* addition)
 
 void Vector2::InitHooks(uintptr_t gameBaseAddress)
 {
+    Memory::InstallHook(gameBaseAddress + ADDR_VECTOR_ADD, FindFunctionAdress(&Vector2::Add), 5);
     Memory::InstallHook(gameBaseAddress + ADDR_VECTOR_MULTIPLY, FindFunctionAdress(&Vector2::Multiply), 5);
     Memory::InstallHook(gameBaseAddress + ADDR_VECTOR_SUM, FindFunctionAdress(&Vector2::Sum), 5);
 }
--- src/Game/Math/Vector2.h
+++ src/Game/Math/Vector2.h
@@ -7,9 +7,11 @@ public:
     float x;
     float y;
 
+    static constexpr uintptr_t ADDR_VECTOR_ADD = 0x9B7C0;
     static constexpr uintptr_t ADDR_VECTOR_MULTIPLY = 0x1F7640;
     static constexpr uintptr_t ADDR_VECTOR_SUM = 0x1F7660;
 
+    Vector2* Add(Vector2* other);
     Vector2* Multiply(Vector2* result, Vector2* operand);
     Vector2* Sum(Vector2* result, Vector2* addition);
 
