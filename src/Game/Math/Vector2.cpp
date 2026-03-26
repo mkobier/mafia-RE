b1df57d74734c9b4e449d0ff9747d90a60c8e626
mkobier <m.kobierecki@wp.pl>
2026-03-26 13:57:14 +0100

    Vector2::AngleBetweenVectors() reversed

--- src/Game/Math/Vector2.cpp
+++ src/Game/Math/Vector2.cpp
@@ -2,6 +2,23 @@
 #include "../../Core/Memory.h"
 #include <iostream>
 
+double __fastcall Vector2::AngleBetweenVectors(Vector2* first, Vector2* second)
+{
+    double result = atan2(second->y, second->x) - atan2(first->y, first->x);
+
+    if (result < -3.1415927)
+    {
+        result += 6.2831855;
+    }
+
+    if (result > 3.1415927)
+    {
+        result -= 6.2831855;
+    }
+
+    return result;
+}
+
 Vector2* Vector2::Add(Vector2* other)
 {
     this->x += other->x;
@@ -28,6 +45,7 @@ Vector2* Vector2::Sum(Vector2* result, Vector2* addition)
 
 void Vector2::InitHooks(uintptr_t gameBaseAddress)
 {
+    Memory::InstallHook(gameBaseAddress + ADDR_ANGLE_BETWEEN_VECTORS, FindFunctionAdress(&Vector2::AngleBetweenVectors), 5);
     Memory::InstallHook(gameBaseAddress + ADDR_VECTOR_ADD, FindFunctionAdress(&Vector2::Add), 5);
     Memory::InstallHook(gameBaseAddress + ADDR_VECTOR_MULTIPLY, FindFunctionAdress(&Vector2::Multiply), 5);
     Memory::InstallHook(gameBaseAddress + ADDR_VECTOR_SUM, FindFunctionAdress(&Vector2::Sum), 5);
